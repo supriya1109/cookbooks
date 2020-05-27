a8193ef348f05d106caf65e1f116b46842065b47
supiya1109 <supriyapandey.100@gmail.com>
2020-05-27 20:07:13 +0000

    version 0.2.0 release -- added support foe emacs to setup.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name 'workstation'
+git push --set-upstream recipes mastername 'workstation'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
